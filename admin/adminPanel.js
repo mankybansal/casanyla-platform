f145f0f900d79ce98dc7606841a5ade170f5d0a1
Mayank Bansal <sunny.bansal@gmail.com>
2016-12-13 21:03:42 +0530

    added templates to admin panel

--- admin/adminPanel.js
+++ admin/adminPanel.js
@@ -1,102 +1,149 @@
- /**
+/**
  * Created by mayankbansal on 6/14/16.
  */
 
 
- var dashboard = true;
+var dashboard = true;
 
- homeluxeApp.controller("adminDashboardControl", function ($scope, $localStorage, $sessionStorage, $rootScope, $interval) {
+homeluxeApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "templates/overview.html",
+            controller: "overviewControl"
+        })
+        .when("/users", {
+            templateUrl: "templates/users.html",
+            controller: "usersControl"
+        })
+        .when("/projects", {
+            templateUrl: "templates/projects.html",
+            controller: "projectsControl"
+        })
+        .when("/designers", {
+            templateUrl: "templates/designers.html",
+            controller: "designersControl"
+        })
+        .when("/clients", {
+            templateUrl: "templates/clients.html",
+            controller: "customersControl"
+        })
+        .when("/styles", {
+            templateUrl: "templates/styles.html",
+            controller: "stylesControl"
+        })
+        .when("/quiz", {
+            templateUrl: "templates/quiz.html",
+            controller: "quizControl"
+        })
+        .when("/help", {
+            templateUrl: "templates/quiz.html",
+            controller: "quizControl"
+        })
+        .when("/settings", {
+            templateUrl: "templates/quiz.html",
+            controller: "quizControl"
+        })
+        .otherwise({
+            templateUrl: "templates/overview.html",
+            controller: "overviewControl"
+        });
+});
 
-     $scope.init = function () {
-       if($localStorage.ngMyUser){
-         $scope.ngMyUser = $localStorage.ngMyUser;
-         showDashboard();
-       } else {
-         $scope.ngMyUser = false;
-         hideDashboard();
-       }
+homeluxeApp.controller("londonCtrl", function ($scope) {
+    $scope.msg = "I love London";
+});
 
-       $scope.users = [];
-       $scope.projects = [];
-       $scope.questionModel = [];
-       $scope.styles = [];
-       $scope.currentEditQuestion = null;
-       $scope.currentEditOption = null;
-       $scope.optionEdit = false;
+homeluxeApp.controller("adminDashboardControl", function ($scope, $localStorage, $sessionStorage, $rootScope, $interval) {
 
+    $scope.init = function () {
+        if ($localStorage.ngMyUser) {
+            $scope.ngMyUser = $localStorage.ngMyUser;
+            showDashboard();
+        } else {
+            $scope.ngMyUser = false;
+            hideDashboard();
+        }
 
-       $scope.requests.showUsers(function(response){
-         $scope.users = response;
-       });
-
-       $scope.requests.getStyles(function(response){
-         $scope.styles = response;
-       })
-
-       $scope.requests.getQuiz(function(response){
-         $scope.questionModel = response;
-       });
-     }
-
-     $scope.editQuestion = function(question){
-          $scope.currentEditQuestion = question;
-     }
-
-     $scope.questionSave = function(){
-
-      var myvar = JSON.parse(angular.toJson($scope.currentEditQuestion));
-       $scope.requests.putQuiz(myvar, $scope.currentEditQuestion._id, function(response){});
-     }
-
-     $scope.editOption = function(option){
-       $scope.optionEdit = true;
-       $scope.currentEditOption = option;
-     }
-
-     $scope.optionSave = function(){
-       //upload script here
-     }
-
-     $scope.logout = function () {
-         $scope.$parent.logout();
-         hideDashboard();
-     };
-
-     $scope.init();
-
- });
+        $scope.users = [];
+        $scope.projects = [];
+        $scope.questionModel = [];
+        $scope.styles = [];
+        $scope.currentEditQuestion = null;
+        $scope.currentEditOption = null;
+        $scope.optionEdit = false;
 
 
+        $scope.requests.showUsers(function (response) {
+            $scope.users = response;
+        });
 
- function showDashboard() {
+        $scope.requests.getStyles(function (response) {
+            $scope.styles = response;
+        })
 
-     $(".viewPanel").hide();
-     $(".menuLeft")
-         .find(".optionSelected").removeClass("optionSelected")
-         .find(".viewOverview").parent().addClass("optionSelected");
-     $("#viewOverview").fadeIn(500);
+        $scope.requests.getQuiz(function (response) {
+            $scope.questionModel = response;
+        });
+    }
 
-     setTimeout(function () {
-         $('.contentOverlay').fadeIn(1000);
-     }, 1000);
- }
+    $scope.editQuestion = function (question) {
+        $scope.currentEditQuestion = question;
+    }
 
- function hideDashboard() {
-     setTimeout(function () {
-         $('.contentOverlay').fadeOut(500);
-     }, 1000);
+    $scope.questionSave = function () {
 
-     setTimeout(function () {
-         $('.loginOverlay').fadeIn(500);
-         showAlert("Successfully logged out.");
-     }, 1500);
- }
+        var myvar = JSON.parse(angular.toJson($scope.currentEditQuestion));
+        $scope.requests.putQuiz(myvar, $scope.currentEditQuestion._id, function (response) {
+        });
+    }
+
+    $scope.editOption = function (option) {
+        $scope.optionEdit = true;
+        $scope.currentEditOption = option;
+    }
+
+    $scope.optionSave = function () {
+        //upload script here
+    }
+
+    $scope.logout = function () {
+        $scope.$parent.logout();
+        hideDashboard();
+    };
+
+    $scope.init();
+
+});
+
+
+function showDashboard() {
+
+    $(".viewPanel").hide();
+    $(".menuLeft")
+        .find(".optionSelected").removeClass("optionSelected")
+        .find(".viewOverview").parent().addClass("optionSelected");
+    $("#viewOverview").fadeIn(500);
+
+    setTimeout(function () {
+        $('.contentOverlay').fadeIn(1000);
+    }, 1000);
+}
+
+function hideDashboard() {
+    setTimeout(function () {
+        $('.contentOverlay').fadeOut(500);
+    }, 1000);
+
+    setTimeout(function () {
+        $('.loginOverlay').fadeIn(500);
+        showAlert("Successfully logged out.");
+    }, 1500);
+}
 
 $(document).ready(function () {
 
 
-
-    $(".menuOption").click(function(){
+    $(".menuOption").click(function () {
         var menuGroupSelected = $(this).parent().find('.menuGroupLabel').text();
         var menuOptionSelected = $(this).find('.menuOptionText').text();
         $('.menuGroupSelected').html(menuGroupSelected);
