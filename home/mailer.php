4689009a2b6e3a21519ec550a196156e595e2025
Mayank Bansal <sunny.bansal@gmail.com>
2017-01-16 21:29:47 -0600

    Fixed Email

--- home/mailer.php
+++ home/mailer.php
@@ -12,6 +12,6 @@ $msg = "First line of text\nSecond line of text";
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,70);
 
-mail("info@casanlya.com","My subject",$msg);
+mail("info@casanyla.com","My subject",$msg);
 
 ?>
