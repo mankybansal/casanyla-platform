c334a8a50c14fa7d9562ac57662dd1503d2dc993
Mayank Bansal <sunny.bansal@gmail.com>
2017-01-16 21:41:08 -0600

    Added API for Mailer

--- home/mailer.php
+++ home/mailer.php
@@ -6,19 +6,28 @@
  * Time: 9:21 PM
  */
 
-$to = "info@casanyla.com";
-$subject = "HTML email";
+if(isset($_GET['to']) && isset($_GET['name']) && isset($_GET['phone']) && isset($_GET['msg'])){
 
-$message = "Test";
+
+    $to = "info@casanyla.com";
+    $subject = "Contact Request";
+
+    $message = "Hello, ".$_GET['name']." has tried to contact you. Phone number: ".$_GET['phone'].". Message: ".$_GET["msg"];
 
 // Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: <noreply@casanyla.com>' . "\r\n";
+    $headers .= 'From: <noreply@casanyla.com>' . "\r\n";
 
-mail($to,$subject,$message,$headers);
-?>
+    mail($to,$subject,$message,$headers);
+
+    echo json_encode(true);
+
+}else{
+    echo json_encode(false);
+}
+
 
 ?>
