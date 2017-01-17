39d0d751367db13a3ae18a13ff455493d6039860
Mayank Bansal <sunny.bansal@gmail.com>
2017-01-16 21:25:37 -0600

    Added Mailer Test

---
+++ home/mailer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mayankbansal
+ * Date: 1/16/17
+ * Time: 9:21 PM
+ */
+
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+mail("info@casanlya.com","My subject",$msg);
+
+?>
