7f924639b13776fbe9c5cfc1d637a6582a8cf6db
Mayank Bansal <sunny.bansal@gmail.com>
2017-01-16 21:36:28 -0600

    Fixed Mailer

--- home/mailer.php
+++ home/mailer.php
@@ -16,7 +16,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: noreply@casanyla.com>' . "\r\n";
+$headers .= 'From: <noreply@casanyla.com>' . "\r\n";
 
 mail($to,$subject,$message,$headers);
 ?>
